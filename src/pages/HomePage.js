b6c3ddaebd8e95a4ef0bd8265ed2a707483bdf9d
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-11-25 13:05:17 +0200

    add hook for home and search form

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,19 @@
 import { HomeList } from 'components/HomeList/HomeList';
 import { Loader } from 'components/Loader/Loader';
 import { fetchTrendingMovies } from 'components/api';
-import { useEffect, useState } from 'react';
+import { useHomeOrSearch } from 'hooks/useHomeOrSearch';
+
 import { useLocation } from 'react-router-dom';
 
 export default function HomePage() {
-  const [movies, setMovies] = useState([]);
   const location = useLocation();
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function getMovies() {
-      try {
-        setLoading(true);
-        setError(false);
-        const topMovies = await fetchTrendingMovies();
-        setMovies(topMovies);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    }
+  const {
+    data: movies,
+    error,
+    loading,
+  } = useHomeOrSearch(fetchTrendingMovies, '');
 
-    getMovies();
-  }, []);
   return (
     <>
       {!error ? (
--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,47 +1,19 @@
 import { Loader } from 'components/Loader/Loader';
 import { SearchedMovies } from 'components/SearchedMovies/SearchedMovies';
 import { fetchSearchMovie } from 'components/api';
-import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+import { useHomeOrSearch } from 'hooks/useHomeOrSearch';
 import { useLocation, useSearchParams } from 'react-router-dom';
 
 export default function MoviesPage() {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
   const location = useLocation();
 
   const [searchParams, setSearchParams] = useSearchParams();
   const queryNew = searchParams.get('query') ?? '';
-
-  useEffect(() => {
-    if (queryNew === '') {
-      return;
-    }
-
-    async function getMovies() {
-      const clearQuery = queryNew.split('/').pop();
-      try {
-        setLoading(true);
-        setError(false);
-
-        const newMovies = await fetchSearchMovie(clearQuery);
-
-        if (newMovies.length === 0) {
-          toast.error('No movie available');
-        } else {
-          setMovies(newMovies);
-        }
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    getMovies();
-  }, [queryNew]);
+  const {
+    data: movies,
+    error,
+    loading,
+  } = useHomeOrSearch(fetchSearchMovie, queryNew.trim());
 
   const onSubmit = searchedQuery => {
     setSearchParams({ query: `${Date.now()}/${searchedQuery.title}` });
